bb31b0ab41c57a20c561a03da08fc2784d4f4782
Speridian <p00997565@asianpaints.com>
2024-09-20 20:56:00 +0530

    init

---
+++ src/main/kotlin/com/asianpaints/apse/service_engineer/ServiceEngineerApplication.kt
@@ -0,0 +1,11 @@
+package com.asianpaints.apse.service_engineer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceEngineerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServiceEngineerApplication>(*args)
+}
